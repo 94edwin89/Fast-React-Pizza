ff3f4a69ea1004b5162db75ba1288f3be033d341
Edwin Jude <edwin.judefrancis@gmail.com>
2024-10-12 17:34:45 +0530

    error handling in form actions

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,6 +1,6 @@
 import { formatCurrency } from "../../utils/helpers";
 
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
+function OrderItem({ item, isLoadingIngredients, ingredients}) {
   const { quantity, name, totalPrice } = item;
 
   return (
