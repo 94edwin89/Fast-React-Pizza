7fc2a014dedf977b1b64851182e2e39dc0ca7835
Edwin Jude <edwin.judefrancis@gmail.com>
2024-10-21 22:07:27 +0530

    using flex-box

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className=" hidden text-sm font-semibold">Edwin</div>;
+  return <div className="hidden text-sm font-semibold md:block">Edwin</div>;
 }
 
 export default Username;
