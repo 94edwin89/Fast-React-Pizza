b4a688199f2cb57c021382c78f9c80b95c638cc3
Edwin Jude <edwin.judefrancis@gmail.com>
2024-10-22 18:35:12 +0530

    styling form elements

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-
-
-
-module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  printWidth: 80, // or whatever line length you prefer
-  singleQuote: true,
-};
