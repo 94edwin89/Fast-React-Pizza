66b0a979ff959fbafda437b67b47352f844bd599
Edwin Jude <edwin.judefrancis@gmail.com>
2024-10-23 21:39:27 +0530

    Button attributes update

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to }) {
   const className =
-    'text stone-800 rounded-full bg-yellow-400 px-4 py-3    font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-yellow-300  focus:bg-yellow-300 focus:outline-none focus:ring      focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+    'text stone-800 rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-yellow-300  focus:bg-yellow-300 focus:outline-none focus:ring  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
   if (to)
     return (
       <Link to={to} className={className}>
